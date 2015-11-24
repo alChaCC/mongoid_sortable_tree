6567353a8a44ebe177840f65bc0e102ad54b14e2
aloha <y.alohac@gmail.com>
2015-11-24 17:59:57 +0800

    [FEA] add create node support and change get 'check' to 'post' avoiding url too long

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tags do 
     collection do 
-      get 'check'
+      post 'check'
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
